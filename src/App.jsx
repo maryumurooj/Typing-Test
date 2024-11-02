c553b28d67bbfdcdfac733ea6a6800c12ad4b0d2
unknown <maryum.urooj05@gmail.com>
2024-11-03 00:42:58 +0530

    adding file

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// App.js
+
+import './App.css';
+import SpeedTypingGame from './components/SpeedTypingGame.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <SpeedTypingGame />
+    </div>
+  );
+}
+
+export default App;
